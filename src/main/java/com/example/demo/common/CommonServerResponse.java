c5e986b808429e38cc6b14888667d5d3ddce8b37
imooc <1179205369@qq.com>
2021-09-25 15:14:08 +0800

    2-2

--- src/main/java/com/example/demo/common/CommonServerResponse.java
+++ src/main/java/com/example/demo/common/CommonServerResponse.java
@@ -6,17 +6,27 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-public class CommonServerResponse{
+public class CommonServerResponse<T>{
     int code;
     String message;
+    T context;
 
     private CommonServerResponse(int code, String message){
         this.code = code;
         this.message = message;
     }
+    private CommonServerResponse(int code, String message,T context){
+        this.code = code;
+        this.message = message;
+        this.context = context;
+    }
 
 
     public static CommonServerResponse setResponse(int code, String message){
         return new CommonServerResponse(code,message);
+    }
+
+    public static <T>CommonServerResponse setResponse(int code, String message,T context){
+        return new CommonServerResponse(code,message,context);
     }
 }
